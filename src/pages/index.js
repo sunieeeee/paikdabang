990377eea8e170c5cd8f7769a3d95962478c9f6b
leeyj <hee396555@gmail.com>
2024-04-30 21:32:39 +0900

    메인페이지 작업중

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import ImageSlider from "react-simple-image-slider";
 import btnPlus from "@/assets/img/main/view_icon.png"
 import btnOn from "@/assets/img/main/btn1-on.png"
 
-import Mainmenu from '@/components/mainmenu';
+// import Mainmenu from '@/components/mainmenu';
 
 const MainContainer = styled.main`
     margin: 0 auto;
@@ -22,15 +22,18 @@ const MainContainer = styled.main`
         .banner_box {
             width: 100%;
             height: 320px;
-            border: 1px solid red;
             background-repeat: no-repeat;
             background-size: cover;
 
             .banner_info {
                 padding: 30px 50px;
-                width: 60%;
-                /* width: 600px; */
-                border: 1px solid red;
+                width: 600px;
+                height: 100%;
+
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+
 
                 &.right_align {
                     padding: 30px 0;
@@ -39,12 +42,10 @@ const MainContainer = styled.main`
 
                 dl {
                     margin-bottom: 40px;
-                    /* border: 1px solid blue; */
                     
                     dt {
-                        margin-bottom: 20px;
-                        width: 20%;
-                        font-size: 60px;
+                        margin-bottom: 30px;
+                        font-size: 52px;
                         font-weight: 900;
                         font-family: var(--font-montserrat);
                     }
@@ -57,15 +58,12 @@ const MainContainer = styled.main`
             }
 
             .btn {
-                /* display: flex;
-                justify-content: center;
-                align-items: center; */
                 position: relative;
                 width: 50px; 
                 height: 50px;
                 background-color: #fff;
                 border-radius: 100%;
-                
+                box-shadow: 0 0px 10px 5px #00000011;
 
                 &:hover {
                     .btnOff {
@@ -102,6 +100,66 @@ const MainContainer = styled.main`
             }
         }
 
+        .banner_hover {
+            background-size: 100% 100%;
+            background-position: center center;
+            transition: .6s;
+            
+            &:hover{
+               background-size: 105% 105%;   
+            }
+
+            dl {
+                dt {
+                    &::after {
+                        content: '';
+                        display: block;
+                        margin-top: 30px;
+                        width: 80px;
+                        height: 3px;
+                        background-color: #000;
+                    }
+                }
+            }
+        }
+
+        .banner_search {
+            display: flex;
+            flex-direction: column;
+            gap: 20px;
+            margin: 20px 0 0 20px;
+            width: 500px;
+            height: auto;
+
+            .banner_info {
+                width: 100%;
+                height: 50%;
+                border: 2px solid #071f60;
+
+                dl {
+                    color: #071f60;
+                    dt {
+                        font-size: 40px;
+                        
+                    }
+                }
+
+                .store_search {
+                    
+                    height: 50px;
+                    border: 2px solid #071f60;
+                    input {
+                        display: block;
+                        padding: 0 10px;
+                        width: 100%; 
+                        height: 100%;
+                        border: 0;
+                        outline: none;
+                    }
+                }
+            }
+        }
+
         // 첫 번째 .banner_box
         & .banner_box:nth-of-type(1) {
             width: 50%;
@@ -110,7 +168,7 @@ const MainContainer = styled.main`
             
             dl {
                 dt {
-                    color: #071f60
+                    color: #071f60;
                 }
                 
                 dd {
@@ -142,15 +200,21 @@ const MainContainer = styled.main`
         // 세 번째 .banner_box
         & .banner_box:nth-of-type(3) {
             width: 100%;
+            height: 600px;
             background-image: url('/assets/img/main/main_sec3.jpg');
 
             .banner_info {
                 margin: 0 auto;
+                width: 1200px;
             }
-
+            
             dl {
                 dt {
                     color: #6b4d30;
+                    
+                    &::after {
+                        background-color: #6b4d30;
+                    }
                 }
                 
                 dd {
@@ -162,11 +226,16 @@ const MainContainer = styled.main`
         // 네 번째 .banner_box
         & .banner_box:nth-of-type(4) {
             width: 50%;
+            height: 600px;
             background-image: url('/assets/img/main/main_sec4.jpg');
 
             dl {
                 dt {
-                    color: #071f60
+                    color: #071f60;
+
+                    &::after {
+                        background-color: #071f60;
+                    }
                 }
                 
                 dd {
@@ -177,15 +246,45 @@ const MainContainer = styled.main`
 
         & .banner_box:nth-of-type(5) {
             width: 50%;
+            height: 600px;
             background-image: url('/assets/img/main/main_sec5.jpg');
 
             dl {
                 dt {
-                    color: #071f60
+                    color: #f1ae1d;
+
+                    &::after {
+                        background-color: #f1ae1d;
+                    }
                 }
                 
                 dd {
-                    color: #293f83;
+                    color: #f1ae1d;
+                }
+            }
+        }
+
+        & .banner_box:nth-of-type(6) {
+            margin-top: 20px;
+            width: 50%;
+            height: 615px;
+            background-image: url('/assets/img/main/main_sec6.jpg');
+
+            .banner_info {
+                width: 600px;
+            }
+
+            dl {
+                dt {
+                    color: #fff;
+
+                    &::after {
+                        background-color: #fff;
+                    }
+                }
+                
+                dd {
+                    color: #fff;
                 }
             }
         }
@@ -204,12 +303,14 @@ const index = memo(() => {
     ]    
 
     const [menuinfo, setMenuinfo] = useState([]);
+    const [storesearch, setStoresearch] = useState([]);
 
     useEffect(() => {
         (async () => {
             try {
                 const response = await axios.get('/data.json');
                 setMenuinfo(response.data.menuinfo);
+                setStoresearch(response.data.storesearch);
                 console.log(menuinfo)
             } catch (error) {
                 console.error('Error fetching data:', error);
@@ -234,23 +335,49 @@ const index = memo(() => {
             <div>
                 <div className="banner_wrap">
                     {menuinfo && menuinfo.map((v, i) => (
-                        <Mainmenu title={v.title} text={v.text} key={i} />
-                        
-                        // <div className="banner_box" key={i}>
-                        //     {/* <a href="">{v.url}</a> */}
-                            
-                        //     <div className="banner_info">
-                        //         <dl>
-                        //             <dt>{v.title}</dt>
-                        //             <dd>{v.text}</dd>
-                        //         </dl>
-                        //         <div className="btn">
-                        //             <Image className="btnOff" src={btnPlus} alt="" />
-                        //             <Image className="btnOn" src={btnOn} alt="" />
-                        //         </div>
-                        //     </div>
-                        // </div>
+                        <div className={i!==0 && i!==1 ? "banner_box banner_hover" : "banner_box" } key={i}>
+                            <div className={i==0 || i==2 || i ==3 || i ==5 ? "banner_info right_align" : "banner_info"}>
+                                <dl>
+                                    <dt>{v.title}</dt>
+                                    <dd>{v.text}</dd>
+                                </dl>
+                                <div className="btn">
+                                    <Image className="btnOff" src={btnPlus} alt="" />
+                                    <Image className="btnOn" src={btnOn} alt="" />
+                                </div>
+                            </div>
+                        </div>
                     ))}
+
+                    <div className="banner_box banner_search">
+                    {storesearch && storesearch.map((v, i) => (
+                        <div className="banner_info" key={i}>
+                        {i === 0 ? (
+                            <>
+                                <dl>
+                                    <dt>{v.title}</dt>
+                                    <dd>{v.text}</dd>
+                                </dl>
+
+                                <div className="store_search">
+                                    <input type="text" name="store_search" placeholder="Find a store in your area!" />
+                                </div>
+                                </>
+                        ) : (
+                            <>
+                                <dl>
+                                    <dt>{v.title}</dt>
+                                    <dd>{v.text}</dd>
+                                </dl>
+                                <div className="btn">
+                                <Image className="btnOff" src={btnPlus} alt="" />
+                                <Image className="btnOn" src={btnOn} alt="" />
+                                </div>
+                            </>
+                        )}
+                        </div>
+                    ))}
+                    </div>
 
                 </div>
             </div>
